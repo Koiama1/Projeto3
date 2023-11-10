08aa4528e800ea9e7a2e443227fff37c1c409796
Koiama1 <114962890+Koiama1@users.noreply.github.com>
2023-11-10 16:12:05 -0300

    Criei o proj.c com os dados do projeto 1

---
+++ proj.c
@@ -0,0 +1,42 @@
+#include "proj.h"
+#include <stdio.h>
+
+//João Pedro Lopes - RA: 24.123.071-3
+//João Pedro Peterutto - RA: 24.123.045-7
+//Gabriel Koiama - RA: 24.123.051-5
+
+void limpa() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) { }
+}
+//função responsável por limpar as informações da variável para poder armazenar novas informações
+
+void ler(struct Tarefa *armazena) {
+    printf("Prioridade: ");
+    scanf("%d", &armazena->prioridade);
+    limpa();
+    printf("Descricao: ");
+    scanf("%299[^\n]", armazena->descricao);
+    limpa();
+    printf("Categoria: ");
+    scanf("%99[^\n]", armazena->categoria);
+    limpa();
+}
+//função responsável por ler as informações do usuário e salvar no arquivo binário
+
+void excluir_tarefa(struct Tarefa *tarefas, int *cont, int posicao) {
+    if (posicao >= 1 && posicao <= *cont) {
+        for (int i = posicao - 1; i < *cont - 1; i++) {
+            tarefas[i] = tarefas[i + 1];
+        }
+        (*cont)--;
+        printf("Tarefa deletada\n\n");
+    } else {
+        printf("Posição inválida!\n\n",*cont);
+        }
+}
+//função responsável por excluir alguma tarefa que está armazenada no arquivo binário
+
+
+
+//A PARTIR DAQUI É O PROJETO 3:
