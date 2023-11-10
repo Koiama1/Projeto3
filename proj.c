068906b3e5ff24e50418da46b86bada759e86254
Koiama1 <114962890+Koiama1@users.noreply.github.com>
2023-11-10 16:52:09 -0300

    proj.c com novas funções não completas

--- proj.c
+++ proj.c
@@ -22,6 +22,9 @@ void ler(struct Tarefa *armazena) {
     printf("Categoria: ");
     scanf("%99[^\n]", armazena->categoria);
     limpa();
+    printf("Estado (completo, em andamento ou não iniciado): ");
+    scanf("%19s", armazena->estado);
+    limpa();
 }
 
 void excluir_tarefa(struct Tarefa *tarefas, int *cont, int posicao) {
@@ -35,7 +38,6 @@ void excluir_tarefa(struct Tarefa *tarefas, int *cont, int posicao) {
         printf("Posição inválida!\n\n");
     }
 }
-
 void alterar_tarefa(struct Tarefa *tarefa, int opcao) {
     switch (opcao) {
         case 1:
@@ -53,7 +55,87 @@ void alterar_tarefa(struct Tarefa *tarefa, int opcao) {
             scanf("%299[^\n]", tarefa->descricao);
             limpa();
             break;
+        case 4:
+            printf("Novo Estado (completo, em andamento ou não iniciado): ");
+            scanf("%19s", tarefa->estado);
+            limpa();
+            break;
         default:
             printf("Opção inválida!\n\n");
+    }
+}
+
+
+
+void filtrar_por_prioridade(struct Tarefa *tarefas, int cont) {
+    int prioridade_filtro;
+    printf("Digite a prioridade para filtrar: ");
+    scanf("%d", &prioridade_filtro);
+    limpa();
+
+    printf("Tarefas com prioridade %d:\n", prioridade_filtro);
+    for (int i = 0; i < cont; i++) {
+        if (tarefas[i].prioridade == prioridade_filtro) {
+            printf("Tarefa %d\n", i + 1);
+            printf("Prioridade: %d\n", tarefas[i].prioridade);
+            printf("Categoria: %s\n", tarefas[i].categoria);
+            printf("Descricao: %s\n\n", tarefas[i].descricao);
+        }
+    }
+}
+
+void filtrar_por_estado(struct Tarefa *tarefas, int cont) {
+    char estado_filtro[20];
+    printf("Digite o estado para filtrar: ");
+    fgets(estado_filtro, sizeof(estado_filtro), stdin);
+    limpa();
+
+    printf("Tarefas com estado %s:\n", estado_filtro);
+    for (int i = 0; i < cont; i++) {
+        if (strcmp(tarefas[i].estado, estado_filtro) == 0) {
+            printf("Tarefa %d\n", i + 1);
+            printf("Prioridade: %d\n", tarefas[i].prioridade);
+            printf("Categoria: %s\n", tarefas[i].categoria);
+            printf("Descricao: %s\n\n", tarefas[i].descricao);
+        }
+    }
+}
+
+void filtrar_por_categoria(struct Tarefa *tarefas, int cont) {
+    char categoria_filtro[100];
+    printf("Digite a categoria para filtrar: ");
+    fgets(categoria_filtro, sizeof(categoria_filtro), stdin);
+    limpa();
+
+    printf("Tarefas na categoria %s:\n", categoria_filtro);
+    for (int i = 0; i < cont; i++) {
+        if (strcmp(tarefas[i].categoria, categoria_filtro) == 0) {
+            printf("Tarefa %d\n", i + 1);
+            printf("Prioridade: %d\n", tarefas[i].prioridade);
+            printf("Categoria: %s\n", tarefas[i].categoria);
+            printf("Descricao: %s\n\n", tarefas[i].descricao);
+        }
+    }
+}
+
+void filtrar_por_prioridade_categoria(struct Tarefa *tarefas, int cont) {
+    int prioridade_filtro;
+    char categoria_filtro[100];
+    printf("Digite a prioridade para filtrar: ");
+    scanf("%d", &prioridade_filtro);
+    limpa();
+
+    printf("Digite a categoria para filtrar: ");
+    fgets(categoria_filtro, sizeof(categoria_filtro), stdin);
+    limpa();
+
+    printf("Tarefas com prioridade %d e categoria %s:\n", prioridade_filtro, categoria_filtro);
+    for (int i = 0; i < cont; i++) {
+        if (tarefas[i].prioridade == prioridade_filtro && strcmp(tarefas[i].categoria, categoria_filtro) == 0) {
+            printf("Tarefa %d\n", i + 1);
+            printf("Prioridade: %d\n", tarefas[i].prioridade);
+            printf("Categoria: %s\n", tarefas[i].categoria);
+            printf("Descricao: %s\n\n", tarefas[i].descricao);
+        }
     }
 }
