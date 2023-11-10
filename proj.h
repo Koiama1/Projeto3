1fcf90f38b56f7c4a88d1dc68553dd57ef1eccf2
Koiama1 <114962890+Koiama1@users.noreply.github.com>
2023-11-10 16:22:06 -0300

    proj.h atualizado com o alterar tarefas

--- proj.h
+++ proj.h
@@ -1,8 +1,11 @@
 #ifndef PROJETO_H
 #define PROJETO_H
 
+//Antigo membros do grupo do projeto 1:
 //João Pedro Lopes - RA: 24.123.071-3
 //João Pedro Peterutto - RA: 24.123.045-7
+
+//Criador desse projeto 3:
 //Gabriel Koiama - RA: 24.123.051-5
 
 struct Tarefa {
@@ -10,17 +13,10 @@ struct Tarefa {
     char categoria[100];
     char descricao[300];
 };
-//Espaço responsável por definir a estrutura da tarefa
 
 void limpa();
-//cabeçalho da função responsável por limpar as informações salvas na variável para poder armazenar novas informações
 void ler(struct Tarefa *armazena);
-//cabeçalho da função que lê as informações dadas pelo usuário e armazena no arquivo
 void excluir_tarefa(struct Tarefa *tarefas, int *cont, int posicao);
-//cabeçalho da função que vai excluir as tarefas
+void alterar_tarefa(struct Tarefa *tarefa, int opcao);
 
-
-//A PARTIR DAQUI É O PROJETO 3:
-
-
-#endif//PROJ.H
+#endif // PROJ.H
