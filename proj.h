fced4bfe619a465bc4130e4b6f08879926657dd2
Koiama1 <114962890+Koiama1@users.noreply.github.com>
2023-11-10 16:51:42 -0300

    proj.h com novas funçoes não completas

--- proj.h
+++ proj.h
@@ -12,11 +12,16 @@ struct Tarefa {
     int prioridade;
     char categoria[100];
     char descricao[300];
+    char estado[20]; // Novo campo para o estado da tarefa
 };
 
 void limpa();
 void ler(struct Tarefa *armazena);
 void excluir_tarefa(struct Tarefa *tarefas, int *cont, int posicao);
 void alterar_tarefa(struct Tarefa *tarefa, int opcao);
+void filtrar_por_prioridade(struct Tarefa *tarefas, int cont);
+void filtrar_por_estado(struct Tarefa *tarefas, int cont);
+void filtrar_por_categoria(struct Tarefa *tarefas, int cont);
+void filtrar_por_prioridade_categoria(struct Tarefa *tarefas, int cont);
 
 #endif // PROJ.H
